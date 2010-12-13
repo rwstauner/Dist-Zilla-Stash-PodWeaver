b08610e4fe3e0e3f6f189fcf795ad0383ac83c3e
Randy Stauner <randy@magnificent-tears.com>
2010-12-13 10:58:25 -0700

    Move more generic attributes and methods to Role::Stash::Plugins
    * argument_separator
    * get_stashed_config
    * merge_stashed_config

--- lib/Dist/Zilla/Role/Stash/Plugins.pm
+++ lib/Dist/Zilla/Role/Stash/Plugins.pm
@@ -11,6 +11,38 @@ with qw(
 
 requires 'expand_package';
 
+=attr argument_separator
+
+A regular expression that will capture
+the package name in C<$1> and
+the attribute name in C<$2>.
+
+Defaults to C<< ^(.+?)\W+(\w+)$ >>
+which means the package variable and the attribute
+will be separated by non-word characters
+(which assumes the attributes will be
+only word characters/valid perl identifiers).
+
+You will need to set this attribute in your stash
+if you need to assign to an attribute in a package that contains
+non-word characters.
+This is an example (taken from the tests in F<t/ini-sep>).
+
+	# dist.ini
+	[%Example]
+	argument_separator = ^([^|]+)\|([^|]+)$
+	-PlugName|Attr::Name = oops
+	+Mod::Name|!goo-ber = nuts
+
+=cut
+
+has argument_separator => (
+    is       => 'ro',
+    isa      => 'Str',
+	# "Module::Name:variable" "-Plugin/variable"
+    default  => '^(.+?)\W+(\w+)$'
+);
+
 =attr _config
 
 Contains the dynamic options.
@@ -56,6 +88,123 @@ around 'BUILDARGS' => sub {
 		%other,
 	}
 };
+
+=method get_stashed_config
+
+Return a hashref of the config arguments for the plugin
+determined by C<< ref($plugin) >>.
+
+This is a slice of the I<_config> attribute
+appropriate for the plugin passed to the method.
+
+	# with a stash of:
+	# _config => {
+	#   'APlug:attr1'   => 'value1',
+	#   'APlug:second'  => '2nd',
+	#   'OtherPlug:attr => '0'
+	# }
+
+	# from inside Dist::Zilla::Plugin::APlug
+
+	my $stashed =
+	Dist::Zilla::Stash::Example->get_stashed_config($self, \%opts);
+
+	# $stashed => {
+	#   'attr1'   => 'value1',
+	#   'second'  => '2nd'
+	# }
+
+Possible options to be included in the hashref:
+
+=for :list
+* I<zilla>
+The current dist-zilla object (which contains the stash).
+
+=cut
+
+sub get_stashed_config {
+	# $class might be $self but it doesn't matter here
+	my ($class, $plugin, $opts) = @_;
+	$opts ||= {};
+	return unless my $zilla = $opts->{zilla};
+	return unless my $stash = $zilla->stash_named($class->stash_name);
+
+	# use ref() rather than $plugin->plugin_name() because we want to match
+	# the full package name as returned by expand_package() below
+	# rather than '@Bundle/ShortPluginName'
+	my $name = ref($plugin);
+
+	my $config = $stash->_config;
+	my $stashed = {};
+	my $splitter = qr/${\ $stash->argument_separator }/;
+
+	while( my ($key, $value) = each %$config ){
+		my ($plug, $attr) = ($key =~ $splitter);
+		my $pack = $class->expand_package($plug);
+
+		$stashed->{$attr} = $value
+			if $pack eq $name;
+	}
+	return $stashed;
+}
+
+=method merge_stashed_config
+
+	Dist::Zilla::Stash::Example->merge_stashed_config($plugin, \%opts);
+
+Get the stashed config (see L</get_stashed_config>),
+then attempt to merge it into the plugin.
+
+This require the plugin's attributes to be writable (C<'rw'>).
+
+It will attempt to push onto array references and
+concatenate onto existing strings (joined by a space).
+It will overwrite any other types.
+
+Possible options:
+
+=for :list
+* I<stashed>
+A hashref like that returned from L</get_stashed_config>.
+If not present, L</get_stashed_config> will be called.
+* I<zilla>
+The current dist-zilla object.
+This is only needed if I<stashed> is not present.
+
+=cut
+
+sub merge_stashed_config {
+	my ($class, $plugin, $opts) = @_;
+	$opts ||= {};
+	my $stashed = $opts->{stashed};
+	return unless $stashed ||= $class->get_stashed_config($plugin, $opts);
+
+	while( my ($key, $value) = each %$stashed ){
+		# call attribute writer (attribute must be 'rw'!)
+		my $attr = $plugin->meta->find_attribute_by_name($key);
+		my $type = $attr->type_constraint;
+		my $previous = $plugin->$key;
+		if( $previous ){
+			if( UNIVERSAL::isa($previous, 'ARRAY') ){
+				push(@$previous, $value);
+			}
+			elsif( $type->name eq 'Str' ){
+				# TODO: pass in string for joining
+				$plugin->$key(join(' ', $previous, $value));
+			}
+			#elsif( $type->name eq 'Bool' )
+			else {
+				$plugin->$key($value);
+			}
+		}
+		else {
+			$value = [$value]
+				if $type->name =~ /^arrayref/i;
+
+			$plugin->$key($value);
+		}
+	}
+}
 
 =method stash_name
 
--- lib/Dist/Zilla/Stash/PodWeaver.pm
+++ lib/Dist/Zilla/Stash/PodWeaver.pm
@@ -14,39 +14,6 @@ package Dist::Zilla::Stash::PodWeaver;
 
 use Pod::Weaver::Config::Assembler ();
 use Moose;
-
-=attr argument_separator
-
-A regular expression that will capture
-the package name in C<$1> and
-the attribute name in C<$2>.
-
-Defaults to C<< ^(.+?)\W+(\w+)$ >>
-which means the package variable and the attribute
-will be separated by non-word characters
-(which assumes the attributes will be
-only word characters/valid perl identifiers).
-
-You will need to set this attribute in your stash
-if you need to assign to an attribute in a package that contains
-non-word characters.
-This is an example (taken from the tests in F<t/ini-sep>).
-
-	# dist.ini
-	[%PodWeaver]
-	argument_separator = ^([^|]+)\|([^|]+)$
-	-PlugName|Attr::Name = oops
-	+Mod::Name|!goo-ber = nuts
-
-=cut
-
-has argument_separator => (
-    is       => 'ro',
-    isa      => 'Str',
-	# "Module::Name:variable" "-Plugin/variable"
-    default  => '^(.+?)\W+(\w+)$'
-);
-
 with 'Dist::Zilla::Role::Stash::Plugins';
 
 =method expand_package
@@ -65,123 +32,6 @@ sub expand_package {
 	# Cannot start an ini line with '='
 	$pack =~ s/^\+/=/;
 	Pod::Weaver::Config::Assembler->expand_package($pack);
-}
-
-=method get_stashed_config
-
-Return a hashref of the config arguments for the plugin
-determined by C<< ref($plugin) >>.
-
-This is a slice of the I<_config> attribute of the Stash
-appropriate for the plugin passed to the method.
-
-	# with a stash of:
-	# _config => {
-	#   '-APlug:attr1'   => 'value1',
-	#   '-APlug:second'  => '2nd',
-	#   '-OtherPlug:attr => '0'
-	# }
-
-	# from inside Pod::Weaver::Plugin::APlug
-
-	my $stashed =
-	Dist::Zilla::Stash::PodWeaver->get_stashed_config($self, \%opts);
-
-	# $stashed => {
-	#   'attr1'   => 'value1',
-	#   'second'  => '2nd'
-	# }
-
-Possible options to be included in the hashref:
-
-=for :list
-* I<zilla>
-The current dist-zilla object (which contains the stash).
-
-=cut
-
-sub get_stashed_config {
-	my ($class, $plugin, $opts) = @_;
-	$opts ||= {};
-	return unless my $zilla = $opts->{zilla};
-	return unless my $stash = $zilla->stash_named('%PodWeaver');
-
-	# use ref() rather than $plugin->plugin_name() because we want to match
-	# the full package name as returned by expand_package() below
-	# rather than '@Bundle/ShortPluginName'
-	my $name = ref($plugin);
-
-	my $config = $stash->_config;
-	my $stashed = {};
-	my $splitter = qr/${\ $stash->argument_separator }/;
-
-	while( my ($key, $value) = each %$config ){
-		my ($plug, $attr) = ($key =~ $splitter);
-		my $pack = $class->expand_package($plug);
-
-		$stashed->{$attr} = $value
-			if $pack eq $name;
-	}
-	return $stashed;
-}
-
-=method merge_stashed_config
-
-	Dist::Zilla::Stash::PodWeaver->get_stashed_config($plugin, \%opts);
-
-Get the stashed config (see L</get_stashed_config>),
-then attempt to merge it into the plugin.
-
-This require the plugin's attributes to be writable (C<'rw'>).
-
-It will attempt to push onto array references and
-concatenate onto existing strings (joined by a space).
-It will overwrite any other types.
-
-Possible options:
-
-=for :list
-* I<stashed>
-A hashref like that returned from L</get_stashed_config>.
-If not present, L</get_stashed_config> will be called.
-* I<zilla>
-The current dist-zilla object.
-This is only needed if I<stashed> is not present.
-
-=cut
-
-sub merge_stashed_config {
-	my ($class) = shift;
-	my ($plugin, $opts) = @_;
-	$opts ||= {};
-	my $stashed = $opts->{stashed};
-	return unless $stashed ||= $class->get_stashed_config($plugin, $opts);
-
-	while( my ($key, $value) = each %$stashed ){
-		# call attribute writer (attribute must be 'rw'!)
-		my $attr = $plugin->meta->find_attribute_by_name($key);
-		my $type = $attr->type_constraint;
-		my $previous = $plugin->$key;
-		if( $previous ){
-			if( UNIVERSAL::isa($previous, 'ARRAY') ){
-				push(@$previous, $value);
-			}
-			elsif( $type->name eq 'Str' ){
-				# TODO: pass in string for joining
-				$plugin->$key(join(' ', $previous, $value));
-			}
-			#elsif( $type->name eq 'Bool' )
-			else {
-				$plugin->$key($value);
-			}
-		}
-		else {
-			$value = [$value]
-				if $type->name =~ /^arrayref/i;
-
-			$plugin->$key($value);
-		}
-	}
 }
 
 1;
@@ -265,7 +115,9 @@ plugin that doesn't support it, please contact that plugin's author(s)
 and let them know about this module.
 
 If you are a L<Pod::Weaver> plugin author,
-have a look at the L</get_stashed_config> and L</merge_stashed_config> methods
+have a look at
+L<Dist::Zilla::Role::Stash::Plugins/get_stashed_config> and
+L<Dist::Zilla::Role::Stash::Plugins/merge_stashed_config>
 to see easy ways to get values from this stash.
 
 Please contact me (and/or send patches) if something doesn't work
